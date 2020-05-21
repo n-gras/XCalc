ad62c78ff6536c494e92079d272b3031dad49d2a
n-gras <niels.gras@outlook.com>
2020-05-21 22:35:18 +0200

    added pipenv pipfile, fixed decimal separator

---
+++ main.py
@@ -0,0 +1,85 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+import re
+
+class MainApp(App):
+    def build(self):
+        self.operators = ["/", "*", "+", "-"]
+        self.last_was_operator = None
+        self.last_button = None
+        main_layout = BoxLayout(orientation="vertical")
+        self.solution = TextInput(
+            multiline=False, readonly=True, halign="right", font_size=55
+        )
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+            ["=", "<-"]
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(
+                    text=label,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
+
+#        equals_button = Button(
+#            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
+#        )
+#        equals_button.bind(on_press=self.on_solution)
+#        main_layout.add_widget(equals_button)
+
+        return main_layout
+
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+        separator = re.split(r"[*/+-]", current) # split solution text into different items per operator to for check decimal separator below
+
+        if button_text == "C":
+            # Clear the solution widget
+            self.solution.text = ""
+        elif button_text == "<-":
+            new_text = current[:-1]
+            self.solution.text = new_text
+        elif button_text == "=":
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+        else:
+            if current and (self.last_was_operator and button_text in self.operators):
+                # Don't add two operators right after each other, instead replace operator
+                new_text = current[:-1] + button_text
+                self.solution.text = new_text
+                return 
+            elif current == "" and button_text in self.operators:
+                # First character cannot be an operator
+                return
+            elif str(separator[-1:]).find(".") != -1 and button_text == ".":
+                # check if decimal separator was already entered- first turn number after last operator into string to use find function
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
+
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
